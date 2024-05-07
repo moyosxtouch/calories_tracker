88a625f44eb559c94c72871659eb204e87a8bec9
moyo <moyosxtouch@gmail.com>
2024-05-07 17:22:01 -0600

    modificaciones en el formulario al submit

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,8 @@
 import { categories } from "../data/categories";
-import { useState, ChangeEvent } from "react";
+import { Activity } from "../types";
+import { useState, ChangeEvent, FormEvent } from "react";
 export default function Form() {
-  const [activity, setActivity] = useState({
+  const [activity, setActivity] = useState<Activity>({
     category: 1,
     name: "",
     calories: 0,
@@ -9,14 +10,25 @@ export default function Form() {
   const handleChange = (
     e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
   ) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id);
     setActivity({
       ...activity,
-      [e.target.id]: e.target.value,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
     });
   };
-
+  const isValidActivity = () => {
+    const { name, calories } = activity;
+    return name.trim() !== "" && calories > 0;
+  };
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submit");
+  };
   return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}
+    >
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">
           Categoria:
@@ -64,8 +76,9 @@ export default function Form() {
       </div>
       <input
         type="submit"
-        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-        value="Guardar Comida o Ejercicio"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+        value={activity.category === 1 ? "Guardar Comida" : "Guardar Ejercicio"}
+        disabled={!isValidActivity()}
       />
     </form>
   );
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,8 @@ export type Category = {
   id: number;
   name: string;
 };
+export type Activity = {
+  category: number;
+  name: string;
+  calories: number;
+};
