724bf1a8e572f8ee789776643a277c16ec5731a3
moyo <moyosxtouch@gmail.com>
2024-05-14 17:59:55 -0600

    se creo el resumen de calorias

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,11 @@
 import { categories } from "../data/categories";
 import { Activity } from "../types";
 import { v4 as uuidv4 } from "uuid";
-import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
-import { ActivityActions } from "../reducers/activity-reducer";
+import { useState, ChangeEvent, FormEvent, Dispatch, useEffect } from "react";
+import { ActivityActions, ActivityState } from "../reducers/activity-reducer";
 type FormProps = {
   dispatch: Dispatch<ActivityActions>;
+  state: ActivityState;
 };
 const initialState: Activity = {
   id: uuidv4(),
@@ -12,8 +13,16 @@ const initialState: Activity = {
   name: "",
   calories: 0,
 };
-export default function Form({ dispatch }: FormProps) {
+export default function Form({ dispatch, state }: FormProps) {
   const [activity, setActivity] = useState<Activity>(initialState);
+  useEffect(() => {
+    if (state.activeId) {
+      const selectActivity = state.activities.filter(
+        (stateActivity) => stateActivity.id === state.activeId
+      )[0];
+      setActivity(selectActivity);
+    }
+  }, [state.activeId]);
   const handleChange = (
     e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
   ) => {
